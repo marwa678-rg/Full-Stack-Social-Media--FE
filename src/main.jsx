76e5372fe8f6a74b4fb2b0294920e7a3318c851c
marwa youssef <marwayoussef678@gmail.com>
2025-12-27 18:56:53 +0200

    Initial React Project Setup

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+
+//Imports
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
+
+//CSS Imports
+import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+
+    <BrowserRouter>
+         <App />
+    </BrowserRouter>
+</Provider>
+</StrictMode>
+)
