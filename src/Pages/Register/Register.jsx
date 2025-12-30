5dc6b42fb4e92493db2978ef7d2ae1e09bdf7eff
marwa youssef <marwayoussef678@gmail.com>
2025-12-30 14:58:19 +0200

    add auth shared styles and verify page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Card, Container, Form } from 'react-bootstrap'
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+//CSS
+import"../../styles/auth.css";
+
 
 export const Register = () => {
+//States
+const[showPass,setShowPass]=useState(false);
+//refs
+
+//handlers
+async function handleSubmit(e){
+  e.preventDefault();
+
+}
+
+
+
   return (
-    <div>Register</div>
+<section className='auth-page'>
+    <Container>
+    <div className='auth-wrapper'>
+    <Card className='auth-card'>
+      <Card.Body>
+        <h3 className='text-center fw-bold mb-2'>
+          Create your account
+        </h3>
+        <p className='text-center text-muted mb-4'>
+          Join and start sharing moments✨
+        </p>
+    </Card.Body>
+<Form onSubmit={handleSubmit}>
+
+  <Form.Group className="mb-3">
+     
+      <Form.Control
+      type="email"
+      name="email"
+      placeholder="Email"
+      required
+      ></Form.Control>
+    </Form.Group>
+
+
+
+
+    <Form.Group className="mb-3">
+      
+      <Form.Control
+      type="text"
+      name="name"
+      placeholder="Your name"
+      required
+      ></Form.Control>
+    </Form.Group>
+
+
+<InputGroup>
+    <Form.Control
+    type={showPass ? "text" :"password"}
+    placeholder="password"
+    required
+    />
+    <InputGroup.Text onClick={()=>{setShowPass(!showPass)}}
+    style={{cursor:"pointer"}}
+    >
+    {showPass ? <FaEye /> : <FaEyeSlash />}    
+    </InputGroup.Text>
+
+</InputGroup>
+
+
+<Button type="submit" className="w-100 auth-btn">
+   Create Account
+ </Button>
+
+</Form>
+
+
+
+
+</Card>
+
+
+    </div>
+
+
+
+
+
+    </Container>
+
+
+
+</section>
   )
 }
--- src/Pages/Verify/Verify.jsx
+++ src/Pages/Verify/Verify.jsx
@@ -1,7 +1,103 @@
-import React from 'react'
+import React, { useRef } from 'react'
+//CSS imports
+import "../../styles/auth.css"
+import { Button, Card, Container, Form } from 'react-bootstrap'
 
 export const Verify = () => {
+//states
+//refs
+const otpRef=useRef(null);
+const emailRef=useRef(null);
+const storedEmail = localStorage.getItem("verifyEmail")
+//_____________________________handlers__________________________________//
+
+//handleSubmit
+async function handleSubmit(e){
+e.preventDefault();
+
+}
+
+//handle resend
+async function handleResend(){
+
+}
+
+
   return (
-    <div>Verify</div>
+<section className='auth-page'>
+    <Container>
+      <div className='auth-wrapper'>
+        <Card className='auth-card'>
+          <Card.Body>
+            <h3 className='text-center fw-bold mb-2'>
+              Verify your email
+            </h3>
+
+    
+            <Form onSubmit={handleSubmit}>
+
+<Form.Group className='mb-4'>
+                    <Form.Control
+                      type="email"
+                      placeholder="Email"
+                      ref={emailRef}
+                      required
+                    />
+                </Form.Group>
+
+
+
+
+
+                <Form.Group className='mb-4'>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter 6-digits code"
+                     ref={otpRef}
+                      required
+                    />
+                </Form.Group>
+
+                <Button type="submit" className='w-100 auth-btn' >
+                    Verify 
+                </Button>
+            </Form>
+        <div className='text-center mt-3'>
+          <small className='text-muted'>
+            Didn't receive the code ? {""}
+            <button
+              onClick={handleResend}
+              style={{
+                border:"none",
+                background:"none",
+                color:"#7c3aed",
+                fontWeight:600,
+              }}>
+                Resnd OTP
+              </button>
+          </small>
+        </div>
+      </Card.Body>
+      </Card>
+    </div>
+</Container>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</section>
   )
 }
