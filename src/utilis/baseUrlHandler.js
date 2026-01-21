9b87dec14be628a8ca9475d4000e831b6e805985
marwa youssef <marwayoussef678@gmail.com>
2026-01-21 16:42:35 +0200

    Fixes BaseUrl Handler remove json.parse

--- src/utilis/baseUrlHandler.js
+++ src/utilis/baseUrlHandler.js
@@ -1,8 +1,10 @@
 
 
 //Get CurrentEnvironment And Set BaseUrl 
+
+
  export function baseUrlHandler(){
-  return JSON.parse(import.meta.env.VITE_PRODUCTION_ENV) ?
+  return import.meta.env.PROD?
   import.meta.env.VITE_BACKEND_BASE :
   import.meta.env.VITE_BACKEND_BASE_LOCAL;
 }
